e2f1f966f1ba5f76ec2a0c2438782fab45b5def9
Younrag <younes.raguebi@usmba.ac.ma>
2023-03-24 00:57:20 -0700

    uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - Checks for uppercase character
+ * @c: caracter
+ * Return: 1 if c is uppercase
+ */
+
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
+
